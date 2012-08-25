6d30f500882318d5f20d2a36f9fcc9d67a0e1efb
Michael McEvoy <mcevoym@musc.edu>
2012-08-25 09:17:05 -0400

    first commit

---
+++ SPARC Test 4/SPARC Test 4/MMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MMAppDelegate.h
+//  SPARC Test 4
+//
+//  Created by Michael McEvoy on 8/25/12.
+//  Copyright (c) 2012 Michael McEvoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MMViewController;
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MMViewController *viewController;
+
+@end
---
+++ SPARC Test 4/SPARC Test 4/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  SPARC Test 4
+//
+//  Created by Michael McEvoy on 8/25/12.
+//  Copyright (c) 2012 Michael McEvoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
